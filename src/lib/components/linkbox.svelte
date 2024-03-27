53401e4fc608a14a0fce28e1b5d2b1a07cc42627
Peter Muthama <muthamapeter79@gmail.com>
2024-03-27 16:30:52 +0300

    fix: minor issues

--- src/lib/components/linkbox.svelte
+++ src/lib/components/linkbox.svelte
@@ -7,9 +7,9 @@
 
   const ENTRANCE_KEYFRAMES = {
     left: [BOTTOM_RIGHT_CLIP, NO_CLIP],
+    bottom: [BOTTOM_RIGHT_CLIP, NO_CLIP],
     top: [BOTTOM_RIGHT_CLIP, NO_CLIP],
     right: [TOP_LEFT_CLIP, NO_CLIP],
-    bottom: [BOTTOM_RIGHT_CLIP, NO_CLIP],
   } as const;
 
   const EXIT_KEYFRAMES = {
@@ -19,7 +19,7 @@
     right: [NO_CLIP, BOTTOM_LEFT_CLIP],
   } as const;
 
-  let clipPath = NO_CLIP
+  let clipPath = NO_CLIP;
   const getNearestSide = (e: MouseEvent) => {
     const box = (e.target as HTMLElement).getBoundingClientRect();
 
@@ -47,10 +47,12 @@
       proximityToBottom,
     ].sort((a, b) => a.proximity - b.proximity);
 
-    return sortedProximity[0].side;
+    return sortedProximity[1].side;
   };
 
-  const handleEnter = (e: MouseEvent & { currentTarget: EventTarget & HTMLAnchorElement; }) => {
+  const handleEnter = (
+    e: MouseEvent & { currentTarget: EventTarget & HTMLAnchorElement }
+  ) => {
     let side = getNearestSide(e);
     clipPath = ENTRANCE_KEYFRAMES[side][0];
   };
@@ -58,26 +60,23 @@
   const handleLeave = (e: MouseEvent) => {
     let side = getNearestSide(e);
     clipPath = EXIT_KEYFRAMES[side][0];
-    
   };
 
-export let Icon:any;
-export let href:any;
+  export let Icon: any;
+  export let href: any;
 </script>
 
-
-  <a href={href}
-    class="relative grid h-20 w-full place-content-center sm:h-28 md:h-36 "
-    on:mouseenter={(e) => handleEnter(e)}
-    on:mouseleave={(e) => handleLeave(e)}
-    
+<a
+  {href}
+  class="relative grid h-20 w-full place-content-center sm:h-28 md:h-36"
+  on:mouseenter={(e) => handleEnter(e)}
+  on:mouseleave={(e) => handleLeave(e)}
+>
+  <Icon class="text-xl sm:text-3xl md:text-4xl" />
+  <div
+    style="clip-path: {clipPath};transition: clip-path 0.5s, background-color 0.5s; "
+    class="absolute inset-0 grid place-content-center bg-neutral-900 text-white"
   >
-  <Icon class="text-xl sm:text-3xl md:text-4xl"/>
-<div
-        style="clip-path: {clipPath};transition: clip-path 0.5s, background-color 0.5s; "
-        class="absolute inset-0 grid place-content-center bg-neutral-900 text-white "
-      >
-        <Icon class="text-xl sm:text-3xl md:text-4xl" />
-      </div>
+    <Icon class="text-xl sm:text-3xl md:text-4xl" />
+  </div>
 </a>
-
